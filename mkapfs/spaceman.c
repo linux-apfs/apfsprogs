ab12638b7adac388387b69f9daf67e4e26759f0d
Ernesto A. Fernández <ernesto.mnd.fernandez@gmail.com>
2019-06-02 17:10:38 -0300

    mkapfs: start work on the spaceman main device
    Create the main device structure, and write the offset of its cibs into
    the spaceman.  The free block count is not set properly yet; that will
    be dealt with along with the allocation bitmap.
    
    Signed-off-by: Ernesto A. Fernández <ernesto.mnd.fernandez@gmail.com>

--- mkapfs/spaceman.c
+++ mkapfs/spaceman.c
@@ -4,6 +4,8 @@
  * Copyright (C) 2019 Ernesto A. Fernández <ernesto.mnd.fernandez@gmail.com>
  */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <apfs/raw.h>
 #include <apfs/types.h>
 #include "mkapfs.h"
@@ -39,6 +41,51 @@ static inline u32 cibs_per_cab(void)
 	return (param->blocksize - cab_size) / sizeof(__le64);
 }
 
+/*
+ * Offsets into the spaceman block for a non-versioned container; the values
+ * have been borrowed from a test image.
+ */
+#define BITMAP_XID_OFF		0x150	/* Transaction id for the ip bitmap */
+#define BITMAP_OFF		0x158	/* Address of the ip bitmap */
+#define BITMAP_FREE_NEXT_OFF	0x160	/* No idea */
+#define CIB_ADDR_BASE_OFF	0x180	/* First cib address for main device */
+
+/**
+ * make_main_device - Make the spaceman main device structure
+ * @sm: pointer to the on-disk spaceman structure
+ */
+static void make_main_device(struct apfs_spaceman_phys *sm)
+{
+	struct apfs_spaceman_device *dev = &sm->sm_dev[APFS_SD_MAIN];
+	u64 chunk_count = DIV_ROUND_UP(param->block_count, blocks_per_chunk());
+	u32 cib_count = DIV_ROUND_UP(chunk_count, chunks_per_cib());
+	__le64 *cib_addr;
+	int i;
+
+	/*
+	 * We must have room for the addresses of all main device cibs, plus
+	 * an extra offset for tier 2.
+	 */
+	if (cib_count + 1 >
+	    (param->blocksize - CIB_ADDR_BASE_OFF) / sizeof(__le64)) {
+		printf("Large containers are not yet supported.\n");
+		exit(1);
+	}
+
+	dev->sm_block_count = cpu_to_le64(param->block_count);
+	dev->sm_chunk_count = cpu_to_le64(chunk_count);
+	dev->sm_cib_count = cpu_to_le32(cib_count);
+	dev->sm_cab_count = 0; /* Not supported, hence the block count limit */
+
+	/* Pretend the whole device is free for now; we'll reduce this later */
+	dev->sm_free_count = cpu_to_le64(param->block_count);
+
+	dev->sm_addr_offset = cpu_to_le32(CIB_ADDR_BASE_OFF);
+	cib_addr = (void *)sm + CIB_ADDR_BASE_OFF;
+	for (i = 0; i < cib_count; ++i)
+		cib_addr[i] = cpu_to_le64(FIRST_CIB_BNO + i);
+}
+
 /**
  * make_spaceman - Make the space manager for the container
  * @bno: block number to use
@@ -52,6 +99,8 @@ void make_spaceman(u64 bno, u64 oid)
 	sm->sm_blocks_per_chunk = cpu_to_le32(blocks_per_chunk());
 	sm->sm_chunks_per_cib = cpu_to_le32(chunks_per_cib());
 	sm->sm_cibs_per_cab = cpu_to_le32(cibs_per_cab());
+
+	make_main_device(sm);
 
 	set_object_header(&sm->sm_o, oid,
 			  APFS_OBJ_EPHEMERAL | APFS_OBJECT_TYPE_SPACEMAN,
